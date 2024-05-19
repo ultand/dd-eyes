2c973bc8c8ecd1cf2e545c620dadc44784e6cb0a
Ultan Daly <43744055+ultand@users.noreply.github.com>
2024-05-19 15:25:10 +0100

    update noise with getter setters

--- src/Noise.py
+++ src/Noise.py
@@ -1,22 +1,51 @@
 import numpy as np
 class Noise:
-    def __init__(self, snr, signal):
-        self.signal = signal
-        self.snr = snr
+    def __init__(self, config=None):
 
-        self.signal_power = 0
-        self._calc_signal_power()
+        config = config or {}
 
-        self.noise = np.zeros_like(signal)
+        self._signal = config.get("signal", None)
+        self._snr = config.get("snr", None)
 
-    def generate_noise(self):
-        snr_linear = 10**(self.snr/10)
-        
-        noise_power = self.signal_power/snr_linear
-        self.noise = np.random.normal(0, np.sqrt(noise_power), self.signal.size)
+        self._signal_power = 0
+        self._noise = np.zeros_like(self._signal)
+
+        self.generate_noise()
+
+    @property
+    def signal(self):
+        return self._signal
+
+    @signal.setter
+    def signal(self, value):
+        self._signal = value
+        self.generate_noise()
     
+    @property
+    def snr(self):
+        return self._snr
+    
+    @snr.setter
+    def snr(self, value):
+        self._snr = value
+        self.generate_noise()
+
+    @property
+    def noise(self):
+        return self._noise
+    
+    @property
+    def signal_power(self):
+        return self._signal_power
+    
+    def generate_noise(self):
+        # efficiency could be improved if I rescaled the noise when updating parmeters instead of recalculating noise every time
+        self._calc_signal_power()
+        snr_linear = 10**(self.snr/10)
+        noise_power = self._signal_power/snr_linear
+        self._noise = np.random.normal(0, np.sqrt(noise_power), self._signal.size)
     
     def _calc_signal_power(self):
         
         mean_power = np.mean(self.signal)
-        self.signal_power = np.mean((self.signal-mean_power) ** 2)
+        self._signal_power = np.mean((self.signal-mean_power) ** 2)
