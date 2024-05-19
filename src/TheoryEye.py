d5ff1ea340994df29237b41f4a55d508a79ebc9a
Ultan Daly <43744055+ultand@users.noreply.github.com>
2024-05-19 21:19:10 +0100

    ber calculation

--- src/TheoryEye.py
+++ src/TheoryEye.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+from scipy.special import erfc
 
 class TheoryEye:
     def __init__(self, config):
@@ -27,6 +28,11 @@ class TheoryEye:
 
         self._plot_data_1 = norm.pdf(self._x_axis, self._data[0], self._noise_variance)
         self._plot_data_2 = norm.pdf(self._x_axis, self._data[1], self._noise_variance)
+
+    def calculated_ber(self):
+        if self._modulation_format.lower() == 'ook':
+            q = 0.5 * erfc(np.sqrt(10**(self._snr/10)/2))
+            print(q)
 
     def plotter(self):
         plt.fill_between(self._x_axis, self._plot_data_1, alpha = 0.3)
