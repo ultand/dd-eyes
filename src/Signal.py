2b157f48fe995085e25bcbba92dd04cc33af5388
Ultan Daly <43744055+ultand@users.noreply.github.com>
2024-05-20 21:54:02 +0100

    document the signal class

--- src/Signal.py
+++ src/Signal.py
@@ -4,9 +4,47 @@ from scipy.signal import butter, filtfilt
 class Signal:
     """
     Class to generate a noiseless Signal with different modulation formats.
+    This class handles the sampling rate and the bandwidth filtering of the data.
+
+    Attributes
+    ----------
+    baud_rate : float
+        The baud rate of the signal.
+    samples : int
+        The number of samples in the signal.
+    bandwidth : float
+        The bandwidth of the signal.
+    sampling_rate : float
+        The sampling rate of the signal.
+    sampled_signal : np.ndarray
+        The sampled version of the signal.
+    signal : np.ndarray
+        The original signal.
+    time_samples : np.ndarray
+        The time samples of the signal.
+    filtered_signal : np.ndarray
+        The filtered version of the signal.
+
+    Methods
+    -------
+    update_processed_signals()
+        Updates both the sampled and filtered versions of the signal.
+    update_sampled_signal()
+        Updates the sampled version of the signal.
+    _calculate_time_samples(signal_length)
+        Calculates the time samples of the signal.
+    update_filtered_signal(filter_order=4)
+        Updates the filtered version of the signal.
     """
     def __init__(self, config=None):
+        """
+        Initializes the Signal class with the provided configuration.
 
+        Parameters
+        ----------
+        config : dict, optional
+            Configuration dictionary with keys 'signal', 'baud_rate', 'bandwidth', and 'sampling_rate'.
+        """
         config = config or {}
 
         #required properties
@@ -28,7 +66,7 @@ class Signal:
         return self._baud_rate
 
     @baud_rate.setter
-    def baud_rate(self, value):
+    def baud_rate(self, value:float):
         self._baud_rate = value
         self.update_processed_signals()
 
@@ -42,7 +80,7 @@ class Signal:
         return self._bandwidth
 
     @bandwidth.setter
-    def bandwidth(self, value):
+    def bandwidth(self, value:float):
         self._bandwidth = value
         self.update_filtered_signal()
 
@@ -51,7 +89,7 @@ class Signal:
         return self._sampling_rate
 
     @sampling_rate.setter
-    def sampling_rate(self, value):
+    def sampling_rate(self, value:float):
 
         self._sampling_rate = value
         self.update_processed_signals()
@@ -61,7 +99,7 @@ class Signal:
         return self._sampled_signal
     
     @sampled_signal.setter
-    def sampled_signal(self, value):
+    def sampled_signal(self, value:np.ndarray):
         self._sampled_signal = value
 
     @property
@@ -71,7 +109,7 @@ class Signal:
         # filter the signal
     
     @signal.setter
-    def signal(self, value):
+    def signal(self, value: np.ndarray):
         # assign time samples based on the baud rate
         self._signal = value
         self._samples = len(value) if value is not None else 0
@@ -88,10 +126,16 @@ class Signal:
         return self._filtered_signal
     
     def update_processed_signals(self):
+        """
+        Updates both the sampled and filtered versions of the signal.
+        """
         self.update_sampled_signal()
         self.update_filtered_signal()
 
     def update_sampled_signal(self):
+        """
+        Updates the sampled version of the signal.
+        """
         if self._baud_rate and (self._signal is not None) and self._samples:
             # note that we round the number of samples per bit to an integer
             # I should create a check and assign this before this point
@@ -102,11 +146,28 @@ class Signal:
             self._calculate_time_samples(total_samples)
 
     def _calculate_time_samples(self, signal_length):
+
+        """
+        Calculates the time samples of the signal.
+
+        Parameters
+        ----------
+        signal_length : int
+            The length of the signal for which to calculate time samples.
+        """
+
         sample_arr = np.linspace(0, signal_length, signal_length)
         self._time_samples = sample_arr/self._sampling_rate
         
     def update_filtered_signal(self, filter_order = 4):
-        # needs to be neatened
+        """
+        Updates the filtered version of the signal.
+
+        Parameters
+        ----------
+        filter_order : int, optional
+            The order of the Butterworth filter to apply (default is 4).
+        """
 
         if self._bandwidth is None:
             print("No Bandwidhth provided...")
