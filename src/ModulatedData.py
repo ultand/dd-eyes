a6106db1b7df1e5747212aa9617fd24136677715
Ultan Daly <43744055+ultand@users.noreply.github.com>
2024-05-20 21:58:16 +0100

    document ModulatedData

--- src/ModulatedData.py
+++ src/ModulatedData.py
@@ -1,7 +1,31 @@
 import numpy as np
 
 class ModulatedData:
+    """
+    Class to generate random modulated data with a specified data format.
+    The data format specifies the number of data levels.
+
+    Attributes
+    ----------
+    modulation_format : str
+        The modulation format of the data (e.g., 'ook').
+    samples : int
+        The number of samples in the data.
+    data : np.ndarray
+        The generated modulated data (read-only).
+
+    Methods
+    -------
+    generate_data()
+        Generate data with the set modulation format and number of samples.
+    """
     def __init__(self, config=None):
+        """
+        Initializes the ModulatedData class with the provided configuration.
+
+        :param config: Dictionary containing the 'modulation_format' and 'samples' initial values. Both values default to None. If no values are provided, they must be set separately before generating data.
+        :type config: dict, optional
+        """
         config = config or {}
 
         self._supported_modulation_format = ['ook']
@@ -55,8 +79,14 @@ class ModulatedData:
         self._data = value
 
     def generate_data(self):
+        """
+        Generate data with the set modulation format and set number of samples.
+        """
         if self.modulation_format.lower() == "ook":
             self._generate_ook()
 
     def _generate_ook(self):
+        """
+        Generate data for On-Off Keying (OOK) modulation format.
+        """
         self.data = np.random.choice([0, 1], self.samples)
