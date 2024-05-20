9f41dbd572ba7f1e38c1ec2aa03e2ea8dfac69fc
Ultan Daly <43744055+ultand@users.noreply.github.com>
2024-05-20 20:16:38 +0100

    update theory eye to getter setter

--- src/TheoryEye.py
+++ src/TheoryEye.py
@@ -7,50 +7,54 @@ class TheoryEye:
     def __init__(self, config):
         config = config or {}
 
-        self._modulation_format = config.get("modulation_format", None)
         self._snr = config.get("snr", None)
         self._plots_x_std = config.get("plots_x_std", 3)
         self._plot_points = config.get("x_axis_samples", 1000)
+        self._data_levels = config.get("data_levels", 2)
 
-        self._data = None
-        self._plot_data = None
+        self._plots_data = None
         self._linear_snr = None
         self._noise_std = None
         self._x_axis = None
         self._ber = None
 
-        self._get_x_axis()
         self._calculate_linear_snr()
-        self._update_noise_variance()
-        self._calculate_ber()
+        self._update_noise_std()
+        self._get_x_axis()
+        self._generate_plot_data()
 
+    @property
+    def data_levels(self):
+        return self._data_levels
+    
+    @data_levels.setter
+    def data_levels(self, value):
+        self._data_levels = value
 
     def _calculate_linear_snr(self):
-        if self._modulation_format.lower() == 'ook':
-            self._linear_snr = 10*np.log10(self._snr/10)
+        if self._data_levels == 2:
+            self._linear_snr = 10**(self._snr/10)
 
-    def _update_noise_variance(self):
-        self._noise_variance = np.sqrt(0.5/self._linear_snr)
+    def _update_noise_std(self):
+        self._noise_std = np.sqrt(0.5/self._linear_snr)
+        self.calculate_ber()
 
     def _get_x_axis(self):
-        min_x = np.min(self._data) - self._plots_x_std * self._noise_std
-        max_x = np.max(self._data) + self._plots_x_std * self._noise_std
+        min_x = 0 - self._plots_x_std * self._noise_std
+        max_x = self._data_levels - 1 + self._plots_x_std * self._noise_std
         self._x_axis = np.linspace(min_x, max_x, self._plot_points)
 
-    def _calculate_ber(self):
-        if self._modulation_format.lower() == 'ook':
-            self._ber = 0.5 * erfc(np.sqrt(self._linear_snr/2))
-            
-    def modulated_data(self):
-        if self._modulation_format.lower() == 'ook':
-            self._data = [0, 1]
-
-    def noisy_data(self):
+    def _generate_plot_data(self):
 
         self._plots_data = []
-        for i, pt in enumerate(self._data):
-            self._plots_data[i] = norm.pdf(self._x_axis, pt, self._noise_std)
+        for i in range(self._data_levels):
+            self._plots_data.append(norm.pdf(self._x_axis, i, self._noise_std))
 
-    def plotter(self):
-        plt.fill_between(self._x_axis, self._plot_data_1, alpha = 0.3)
-        plt.fill_between(self._x_axis, self._plot_data_2, alpha = 0.3, color = 'orange')
+    def calculate_ber(self):
+        if self._data_levels == 2:
+            self._ber = 0.5 * erfc(np.sqrt(self._linear_snr/2))
+
+    def plotter(self, ax):
+        for plot_data in self._plots_data:
+            plt.fill_between(self._x_axis, plot_data, alpha = 0.3)
+        return ax
