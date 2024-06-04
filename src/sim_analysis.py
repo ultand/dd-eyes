35eae59228a89e2f73c21a7a7cd0026c4704207e
Ultan Daly <43744055+ultand@users.noreply.github.com>
2024-06-04 23:28:01 +0100

    determine the transition time and points to remove for analysis

--- src/sim_analysis.py
+++ src/sim_analysis.py
@@ -3,14 +3,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class SimAnalysis:
-    def __init__(self):
+    def __init__(self, config = None):
         self.params = []
+        self._bandwidth = None
 
-    def _remove_transition_times(self):
+
+    @property
+    def bandwidth(self):
+        return self._bandwidth
+
+    @bandwidth.setter
+    def bandwidth(self, value):
+        self._bandwidth = value
+
+    def _remove_transition_times(self, signal, sampling_time, baud_rate, samples):
         """
         based on the signal bandwidth, this remove the transition regions from the signal such that only the signal levels are analysed
         """
-        pass
+        # this will require the signal
+        # the bandwidth
+        # the 90% - 10% rise time is assumed to be 0.35/3dB bandwidth
+        # https://www.thorlabs.com/images/TabImages/Rise_Time_3dB_Bandwidth_Relationship_Lab_Fact.pdf
+
+        transition_time = 0.35/self.bandwidth
+
+        #determine the points at which the transition happens
+        # currently this is in sample units, not real space
+        bit_samples = int(1/(baud_rate * sampling_time))
+        transition_points = [ x*bit_samples for x in range(samples)]
+
+        #determine the number of points that will need to be removed from each transition point. Note that this should be centered on each point
+        transition_range = transition_time/sampling_time
+
+
 
     def add_to_params(self, mu, sigma, A):
         self.params.extend([mu, sigma, A])
