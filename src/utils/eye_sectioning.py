441c985e34958f86f589d9af0b25b8a7845dc9c8
Ultan Daly <43744055+ultand@users.noreply.github.com>
2024-09-17 23:12:17 +0100

    fix tabss

--- src/utils/eye_sectioning.py
+++ src/utils/eye_sectioning.py
@@ -2,16 +2,16 @@ import numpy as np
 
 
 def gen_eye_time_samples(eye_realisation_samps, sample_rate, eye_realisations):
-        """
-        Generates the time samples for the eye diagram.
-        This allows the signal to be partioned to produce the eye diagram 
-        """
-        true_time_stamps = np.linspace(0, 
-                                       eye_realisation_samps, eye_realisation_samps)
-        true_time_stamps /= sample_rate
+    """
+    Generates the time samples for the eye diagram.
+    This allows the signal to be partioned to produce the eye diagram 
+    """
+    true_time_stamps = np.linspace(0, 
+                                    eye_realisation_samps, eye_realisation_samps)
+    true_time_stamps /= sample_rate
 
-        eye_time_samples = np.tile(true_time_stamps, eye_realisations)
-        return eye_time_samples
+    eye_time_samples = np.tile(true_time_stamps, eye_realisations)
+    return eye_time_samples
 
 def used_eye_points(signal, eye_length, eye_realisation_samps):
     
